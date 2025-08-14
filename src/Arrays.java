320af00180e780e51eb82549c95c8f525a32a0b3
RiteshYadav1926 <ritesh192624@gmail.com>
2025-08-14 22:47:59 +0530

    Create Arrays.java
    Instantiating an Array

---
+++ src/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        int[] myIntArray = new int[10];
+        myIntArray[0] = 45;
+        myIntArray[5] = 10;
+
+        double[] myDoubleArray = new double[6];
+        myDoubleArray[3] = 9.578;
+
+        System.out.println(myIntArray[0]);
+        System.out.println(myDoubleArray[3]);
+    }
+}
