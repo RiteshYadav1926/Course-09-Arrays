c7d85c8e3d30a04a6daaa1d6c1af4022e87c5e89
RiteshYadav1926 <ritesh192624@gmail.com>
2025-08-15 00:39:27 +0530

    Update HelperClassProperties.java
    1. Printing Arrays Elements
    2. Sorting in Ascending Order

--- src/HelperClassProperties.java
+++ src/HelperClassProperties.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class HelperClassProperties {
     public static void main(String[] args) {
+
+        int[] firstArray = getRandomArray(10);
+        System.out.println(Arrays.toString(firstArray));
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
 
     }
     public static int[] getRandomArray(int len){
