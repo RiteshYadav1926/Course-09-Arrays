cee7f4a065cb9012c7d3d534dbd40c472c814a19
RiteshYadav1926 <ritesh192624@gmail.com>
2025-08-16 16:59:07 +0530

    Create Challenge.java
    Taking Input from User
    Conveerting that String into Int Array

---
+++ src/Challenge.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Challenge {
+    public static void main(String[] args) {
+
+        readIntegers();
+
+    }
+    public static int[] readIntegers(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter all the Elements of Array separated with Comma's - ");
+        String elements = scanner.nextLine();
+
+        String[] myArray = elements.split(",");
+        int[] array = new int[myArray.length];
+        for (int i = 0; i < myArray.length; i++) {
+            array[i] = Integer.parseInt(myArray[i]);
+        }
+        System.out.println(Arrays.toString(array));
+        return array;
+    }
+}
