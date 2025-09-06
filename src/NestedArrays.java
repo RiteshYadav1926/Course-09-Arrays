6a971b4e71ec35e96cb26a10f95e55ddd1c724fe
RiteshYadav1926 <ritesh192624@gmail.com>
2025-09-06 17:10:24 +0530

    Create NestedArrays.java
    Calling toString Method to Print out the Array
    But here default values will be printed out

---
+++ src/NestedArrays.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class NestedArrays {
+    public static void main(String[] args) {
+
+        int[][] array1 = new int[4][4];
+        System.out.println(Arrays.toString(array1));
+        System.out.println("Length of Array = " + array1.length);
+    }
+}
